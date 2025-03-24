f97465e36d944a64de170c1717ef1fd13be2b9a8
Udit Rajput <87136581+uditrajput03@users.noreply.github.com>
2025-03-24 21:45:27 +0530

    .

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import { GetRedis } from "./getRedis";
 
-console.log("Hello, World!");
-const client = GetRedis.getInstance().getClient();
+function main() {
 
-const username = "";
-const imageName = ""
-const withTagName = ""
+    const client = GetRedis.getInstance().getClient();
 
-//To scane for all the Images of a user i.e scan for all the images of the username with respective tags
-client.lPush('usersource', username)
+    // const username = "";
+    const imageName = "uditrajput03/image-containing-env"
+    // const withTagName = ""
 
-//To scane for Image with Name i.e scan for all the tags of the image
-client.lPush('imagesource', imageName)
+    //To scane for all the Images of a user i.e scan for all the images of the username with respective tags
+    // client.lPush('usersource', username)
 
-//To scane for Image with Given Tag
-client.lPush('workersource', withTagName)
+    //To scane for Image with Name i.e scan for all the tags of the image
+    client.lPush('imagesource', imageName)
+
+    //To scane for Image with Given Tag
+    // client.lPush('workersource', withTagName)
+}
+main();
